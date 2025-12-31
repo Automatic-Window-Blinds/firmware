96f95289be23abd7c850a98e25f899ba09906682
Dylan Parks <dylan@bouwerbytes.com>
2025-12-31 11:33:57 -0600

    Improved UART Init

--- src/board/hal_msp.cpp
+++ src/board/hal_msp.cpp
@@ -7,13 +7,14 @@
 // This is called automatically by HAL_UART_Init()
 // ----------------------------------------------------------------------------
 extern "C" void HAL_UART_MspInit(UART_HandleTypeDef* huart) {
-    if (reinterpret_cast<std::uintptr_t>(huart->Instance) != board::pins::kConsoleUartBase) return;
+    if (huart->Instance != reinterpret_cast<USART_TypeDef*>(board::pins::kConsoleUartBase)) return;
+
+    __HAL_RCC_USART2_CLK_ENABLE();
 
     board::pins::ConsoleTx::ConfigureAlternate(GPIO_AF7_USART2, hal::OutputType::PushPull, hal::Speed::Medium,
                                                hal::Pull::None);
     board::pins::ConsoleRx::ConfigureAlternate(GPIO_AF7_USART2, hal::OutputType::PushPull, hal::Speed::Medium,
                                                hal::Pull::None);
-    __HAL_RCC_USART2_CLK_ENABLE();
 }
 
 #endif  // CPPCHECK
