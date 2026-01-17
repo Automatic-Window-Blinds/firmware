20e10f2f7dad20fc3c3c23935f8ef3342b5942b9
Dylan Parks <dylan@bouwerbytes.com>
2026-01-16 19:57:50 -0600

    Add EXTI9_5 interrupt handler and configure GPIO for falling edge trigger

--- boards/nucleo_l432kc/Core/Src/gpio.c
+++ boards/nucleo_l432kc/Core/Src/gpio.c
@@ -61,9 +61,13 @@ void MX_GPIO_Init(void)
 
   /*Configure GPIO pin : PB5 */
   GPIO_InitStruct.Pin = GPIO_PIN_5;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
 
 }
 
