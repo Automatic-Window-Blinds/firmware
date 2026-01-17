9bb3eda3b2a77a4ed17b020f6e60a36977a27039
Dylan Parks <dylan@bouwerbytes.com>
2026-01-16 19:45:33 -0600

    Add support for Nucleo L432KC board. ADC, DAC, USART all functional.

---
+++ include/boards/nucleo_l432kc/board_defs.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <stm32l4xx_hal.h>
+
+#include <cstdint>
+
+#include "hal/fast_gpio.hpp"
+
+namespace board::pins {
+
+using ConsoleTx = hal::FastGpio<GPIOA_BASE, GPIO_PIN_2>;
+using ConsoleRx = hal::FastGpio<GPIOA_BASE, GPIO_PIN_3>;
+
+inline constexpr std::uintptr_t CONSOLE_UART_BASE = USART2_BASE;
+
+using StatusLed = hal::FastGpio<GPIOB_BASE, GPIO_PIN_3>;
+using UserButton = hal::FastGpio<GPIOB_BASE, GPIO_PIN_5>;
+
+}  // namespace board::pins
